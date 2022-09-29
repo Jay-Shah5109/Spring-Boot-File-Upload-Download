763c177374ac43a6a36c880326554f2660c1a1fb
jay <jayshah5109@gmail.com>
2022-09-29 18:53:21 +0530

    added controller methods and config properties for db

--- src/main/java/com/mycode/storageservice/StorageServiceApplication.java
+++ src/main/java/com/mycode/storageservice/StorageServiceApplication.java
@@ -1,13 +1,42 @@
 package com.mycode.storageservice;
 
+import com.mycode.storageservice.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/image")
 public class StorageServiceApplication {
+
+	@Autowired
+	private StorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StorageServiceApplication.class, args);
+	}
+
+	@PostMapping
+	public ResponseEntity<?> uploadImage(@RequestParam("image")MultipartFile multipartFile) throws IOException {
+		String uploadImage = storageService.uploadImage(multipartFile);
+		return ResponseEntity.status(HttpStatus.OK).body(uploadImage);
+	}
+
+
+	@GetMapping("/{fileName}")
+	public ResponseEntity<?> downloadImage(@PathVariable String fileName) {
+		byte[] image = storageService.downloadImage(fileName);
+		return ResponseEntity.status(HttpStatus.OK)
+				.contentType(MediaType.valueOf("image/png"))
+				.body(image);
 	}
 
 }
